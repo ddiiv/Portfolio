529154586ba1d9e9c0bb8be4c13a3c2602c21d50
dantolin45 <dantolin45@gmail.com>
2023-10-30 10:41:25 -0300

    initial commit

---
+++ src/pages/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Main = () => {
+
+    return (
+        <>
+            <div className="page">
+                <div className="pagecontainer">
+
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Main;
